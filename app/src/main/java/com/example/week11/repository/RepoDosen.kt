20af15c4ec68e38dfdbb8e0749371ebb945ebf52
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-12-21 15:36:34 +0700

    menambahkan interface RepoDosen untuk insert dan ambil daftar Dosen

--- app/src/main/java/com/example/week11/repository/RepoDosen.kt
+++ app/src/main/java/com/example/week11/repository/RepoDosen.kt
@@ -1,8 +1,9 @@
 package com.example.week11.repository
 
 import com.example.week11.data.entity.Dosen
+    import kotlinx.coroutines.flow.Flow
 
 interface RepoDosen {
     suspend fun insertDosen(dosen: Dosen)
-
+    fun getAllDosen(): Flow<List<Dosen>>
 }

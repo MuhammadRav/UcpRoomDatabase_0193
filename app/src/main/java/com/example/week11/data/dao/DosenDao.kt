d17fe1816bbd58b4affde112cbe67922a01adba6
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-12-21 15:50:24 +0700

    menambahkan implementasi untuk mengambil semua data Dosen dari database

--- app/src/main/java/com/example/week11/data/dao/DosenDao.kt
+++ app/src/main/java/com/example/week11/data/dao/DosenDao.kt
@@ -2,10 +2,14 @@ package com.example.week11.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.week11.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DosenDao {
     @Insert
     suspend fun insertDosen(dosen: Dosen)
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen(): Flow<List<Dosen>>
 }
--- app/src/main/java/com/example/week11/repository/LocalRepoDosen.kt
+++ app/src/main/java/com/example/week11/repository/LocalRepoDosen.kt
@@ -2,11 +2,15 @@ package com.example.week11.repository
 
 import com.example.week11.data.dao.DosenDao
 import com.example.week11.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepoDosen(
     private val dosenDao: DosenDao
 ): RepoDosen {
     override suspend fun insertDosen(dosen: Dosen) {
         dosenDao.insertDosen(dosen)
+    }
+    override fun getAllDosen(): Flow<List<Dosen>> {
+        return dosenDao.getAllDosen()
     }
 }

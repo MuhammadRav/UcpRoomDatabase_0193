35c808bed59e21202205ce41fdc626402a25b29d
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-12-22 13:17:45 +0700

    membuat tampilan DetailMatakuliahView dengan fitur edit dan hapus data

--- app/src/main/java/com/example/week11/ui/view/matakuliah/DetailMkView.kt
+++ app/src/main/java/com/example/week11/ui/view/matakuliah/DetailMkView.kt
@@ -1,2 +1,231 @@
 package com.example.week11.ui.view.matakuliah
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.week11.data.entity.Matakuliah
+import com.example.week11.ui.costumwidget.CstTopAppBar
+import com.example.week11.ui.viewModel.matakuliahViewModel.DetailMatakuliahViewModel
+import com.example.week11.ui.viewModel.matakuliahViewModel.DetailUiState
+import com.example.week11.ui.viewModel.matakuliahViewModel.PenyediaMatakuliahViewModel
+import com.example.week11.ui.viewModel.matakuliahViewModel.toMatakuliahEntity
+
+@Composable
+fun DetailMatakuliahView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailMatakuliahViewModel = viewModel(factory = PenyediaMatakuliahViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+){
+    Scaffold (
+        topBar = {
+            CstTopAppBar(
+                judul = "Detail Matakuliah",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUiState.value.detailUiEvent.kodeMk)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Matakuliah",
+                )
+            }
+        }
+    ){ innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailMatakuliah(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteMatakuliah()
+                onDeleteClick()
+            }
+        )
+    }
+}
+
+@Composable
+fun BodyDetailMatakuliah(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = { },
+){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        detailUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailMatakuliah(
+                    matakuliah = detailUiState.detailUiEvent.toMatakuliahEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if (deleteConfirmationRequired){
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false},
+                        modifier= Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+        detailUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemDetailMatakuliah(
+    modifier: Modifier = Modifier,
+    matakuliah: Matakuliah
+){
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailMatakuliah(judul = "Kode Matakuliah", isinya = matakuliah.kodeMk)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailMatakuliah(judul = "Nama Matakuliah", isinya = matakuliah.namaMk)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailMatakuliah(judul = "SKS", isinya = matakuliah.sks)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailMatakuliah(judul = "Semester", isinya = matakuliah.semester)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailMatakuliah(judul = "Jenis Matakuliah", isinya = matakuliah.jenisMk)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailMatakuliah(judul = "Dosen Pengampu", isinya = matakuliah.dosenPengampu)
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailMatakuliah(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+
+        horizontalAlignment = Alignment.Start
+    ){
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
+}

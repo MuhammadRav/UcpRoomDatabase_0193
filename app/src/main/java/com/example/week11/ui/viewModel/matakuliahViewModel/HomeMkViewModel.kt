2235d9d854059f9fd268195872b312f2d5655034
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-12-22 13:41:38 +0700

    menambahkan ViewModel dan UI State untuk halaman Home

--- app/src/main/java/com/example/week11/ui/viewModel/matakuliahViewModel/HomeMkViewModel.kt
+++ app/src/main/java/com/example/week11/ui/viewModel/matakuliahViewModel/HomeMkViewModel.kt
@@ -1,2 +1,54 @@
 package com.example.week11.ui.viewModel.matakuliahViewModel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.week11.data.entity.Matakuliah
+import com.example.week11.repository.RepoMatakuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+data class  HomeUiState(
+    val listMatakuliah: List<Matakuliah> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+
+class HomeMatakuliahViewModel (
+    private val repoMatakuliah: RepoMatakuliah
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repoMatakuliah.getAllMatakuliah()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMatakuliah = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}

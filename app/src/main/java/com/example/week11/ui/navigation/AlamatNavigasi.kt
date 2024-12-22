1632b4beb51e4ddb2d146dcefc19b2465a7d0a14
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-12-22 09:48:06 +0700

    membuat objek navigasi untuk Home, Detail, dan Update

--- app/src/main/java/com/example/week11/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/week11/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,18 @@ package com.example.week11.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val KODEMK = "kodeMk"
+    val routesWithArg = "$route/{$KODEMK}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val KODEMK = "kodeMk"
+    val routesWithArg = "$route/{$KODEMK}"
+}
